da9f974da1d767d91261110574d0bc482e9697e0
ManasiSonawane <136892797+ManasiSonawane@users.noreply.github.com>
2024-12-13 11:22:25 +0530

    mmm
    dekh

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 
 const path = require('path');
+const methodOverride = require("method-override");
+const ejsMate = require ("ejs-mate");
 const app = express();
 const PORT = 8080;
 const MONGO_URL =
@@ -26,8 +28,11 @@ async function main() {
 }
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
-
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
 // Routes
 // Root route
 app.get("/", (req, res) => {
@@ -61,11 +66,16 @@ app.get("/", (req, res) => {
   }
 });*/
 
-// Fetch all listings
+// Fetch all listings  index route
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
   });
+  //New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
   //Show Route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
@@ -73,7 +83,32 @@ app.get("/listings/:id", async (req, res) => {
     res.render("listings/show.ejs", { listing });
   });
   
+//Create Route
+/*app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});*/
 
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
