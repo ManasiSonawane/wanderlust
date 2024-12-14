eef4874ff4cd39331db17eba3dfd6a5474019032
ManasiSonawane <136892797+ManasiSonawane@users.noreply.github.com>
2024-12-14 14:33:29 +0530

    m
    m

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-//const wrapAsync = require("./utils/wrapAsync.js");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 const app = express();
 const PORT = 8080;
 const MONGO_URL =
@@ -68,70 +69,81 @@ app.get("/", (req, res) => {
 });*/
 
 // Fetch all listings  index route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync (async(req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
-});
+}));
 //New Route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync( async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
-});
-//Create Route
+}));
+//Create Route ara
 
-app.post("/listings", async (req, res) => {
-  try {
+//app.post("/listings", async (req, res) => {
+  //try {
+    //const newListing = new Listing(req.body.listing);
+    //console.log(newListing);
+    //await newListing.save();
+    //res.redirect("/listings");
+  //} catch (error) {
+    //console.error("Error creating listing:", error);
+    //res.status(500).send("Failed to create listing");
+ // }
+//});
+
+//Create Route
+ app.post("/listings", wrapAsync(async (req, res, next) => {
+   if(!req.body.listing){
+    throw new ExpressError(400,"Send valid data for listing")
+   }
     const newListing = new Listing(req.body.listing);
-    console.log(newListing);
     await newListing.save();
-    res.redirect("/listings");
-  } catch (error) {
-    console.error("Error creating listing:", error);
-    res.status(500).send("Failed to create listing");
-  }
-});
+     res.redirect("/listings");
 
-//Create Route
-// app.post("/listings", async (req, res, next) => {
-//   try {
-//     const newListing = new Listing(req.body.listing);
-//     await newListing.save();
-//     res.redirect("/listings");
-//   } catch (err) {
-//     console.log(err);
-//     next(err);
-//   }
-// });
+  
+}));
 
 //Edit Route
 
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+  if(!req.body.listing){
+    throw new ExpressError(400,"Send valid data for listing")
+   }
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
-});
+}));
+
+
+app.all("*",(req,res,next)=>{
+  next(new ExpressError(404,"Page not found"));
+})
+
 app.use((err, req, res, next) => {
-  res.send("Somethimg went wrong");
+ let {statusCode=500, message="Something went wrong"}= err;
+ res.status(statusCode).render("error.ejs",{message});
+ //res.status(statusCode).send(message);
 });
 // Start the server
 app.listen(PORT, () => {
