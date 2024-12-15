1d27a892d3ee3232d9595f0ab0cbe4588fa989ed
ManasiSonawane <136892797+ManasiSonawane@users.noreply.github.com>
2024-12-15 23:23:02 +0530

    m

--- app.js
+++ app.js
@@ -9,14 +9,18 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const Review = require("./models/review.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+
+
 
 const app = express();
 const PORT = 8080;
@@ -46,15 +50,15 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  secret:"mysupersecretcode",
   resave: false,
   saveUninitialized: true,
-  cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-  },
-};
+  cookie:{
+  expires:Date.now()+7 * 24 * 60* 60 * 1000,
+  maxAge:7 * 24 * 60* 60 * 1000,
+  httpOnly: true,
+},
+} ;
 
 // Routes
 // Root route
@@ -71,24 +75,25 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) => {
+app.use((req, res,next )=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  next();
-});
+ next();
+})
 
-app.get("/demouser", async (req, res) => {
-  let fakeUser = new User({
-    email: "student@gmail.com",
-    username: "delta-Student",
-  });
-  let registeredUser = await User.register(fakeUser, "helloworld");
-  res.send(registeredUser);
-});
+//app.get("/demouser" ,async (req, res) =>{
+//let fakeUser = new User({
+  //email: "student@gmail.com",
+  //username: "delta-Student"
+//});
+//let registeredUser= await User.register(fakeUser,"helloworld");
+//res.send(registeredUser);
+//});
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
+
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found"));
 });
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,70 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+//const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
+
+router.get("/signup", wrapAsync(async(req, res) => {
+res.render("users/signup.ejs");
 });
+router.post("/signup",async( req, res) =>{
+let{username,email,password} =req.body;
+try{
+    const newUser = new User({email,username});
+const registeredUser = await User.registor(newUser, password);
+console.log (registeredUser);
+req.flash("success","welcome to SmartTrade");
+res.redirect("/listings")
+}catch(e){
+    req.flash("error",e.message);
+    res.redirect("/signup");
+}
+})
+);
+
+router.get("/login",(req,res) =>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local" ,{
+    failureRedirect: "/login", 
+    failureFlash: true}),
+ async(req, res) => {
+    res.send("Welcome to SmartTrade! you are logged in");
+    });
+
+/*module.exports = router;
+const userContorller = require("../controllers/users.js");
+
+
+router
+    .route("/signup")
+ 
+    .get( userContorller.rendersignupForm)
+
+  
+    .post(wrapAsync( userContorller.signup ));
+
+
+router
+    .route("/login")
+ 
+    .get( userContorller.renderLoginForm)
+
+   
+    .post( saveRedirectUrl,
+        passport.authenticate("local", {
+            failureRedirect: "/login", 
+            failureFlash: true,
+        }), 
+        userContorller.login
+    );
+
+
+router.get("/logout", userContorller.logout);
+
+*/
 module.exports = router;
